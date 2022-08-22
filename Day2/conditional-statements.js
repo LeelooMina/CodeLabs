af6f938a599c3129f1c8ba02c7e05735743e9aec
Crystal Campbell <blankpage@gmail.com>
2022-08-22 15:43:05 -0500

    ADD conditional statements + for loop

---
+++ Day2/conditional-statements.js
@@ -0,0 +1,9 @@
+let passWord = "CodeLabs"
+
+let inputPass = prompt("What is the password?")
+
+if (inputPass == passWord){
+console.log("Password correct!")
+} else {
+console.log("Sorry that password is not correct.")
+}

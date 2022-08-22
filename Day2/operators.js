c3c8f9bdca92e52021cbe38a5af51d73ab1192e7
Crystal Campbell <blankpage@gmail.com>
2022-08-22 15:15:19 -0500

    ADD Homework

---
+++ Day2/operators.js
@@ -0,0 +1,31 @@
+let addThis = 1 + 1;
+
+let subtractThis = 5 - 1;
+
+let DivideThis = 6 / 1;
+
+let multiplyThis = 5 * 5;
+
+let greaterThan = 5;
+
+if (greaterThan > 2) {
+    console.log("Hello");
+}
+
+if (greaterThan < 2){
+    console.log("Nope");
+}
+
+if (greaterThan === 5){
+    console.log("Yup");
+}
+
+if (greaterThan !== 10){
+    console.log("Hi")
+}
+
+
+
+
+
+
---
+++ Day2/variables.js
@@ -0,0 +1,64 @@
+// String variable
+const stringVar = "Hello!";
+
+//Print to console
+console.log(stringVar);
+
+//Number variable
+const numVar = 55;
+
+//Print to console
+console.log(numVar);
+
+//Boolean variable
+const boolean = true;
+
+//Print to console
+console.log(boolean);
+
+//Null variable
+const nullTest = null;
+
+//Print to console
+console.log(nullTest);
+
+//Undefined variable
+const undefinedTest = undefined;
+
+//Print to console
+console.log(undefinedTest);
+
+//Object 
+const animalAttributes = {
+    size: "Large", 
+    color: "Blue", 
+    name: "Elephant"};
+
+//Variable that points to object instance
+const animalType = animalAttributes.name;
+
+//Print to console
+console.log(animalType);
+
+//Array
+const animalList = [
+    'Cat',
+    'Dog',
+    'Fish'
+];
+
+//Array variable
+const animalName = animalList[2];
+
+//Print to console
+console.log(animalName);
+
+//No default value
+let noValuesHere;
+
+//Print to console
+console.log(noValuesHere);
+
+
+
+

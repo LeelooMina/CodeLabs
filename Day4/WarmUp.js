1c38c940e5d31f18e34a70e89d823d3b58da39eb
Crystal Campbell <blankpage@gmail.com>
2022-08-25 17:59:02 -0500

    Update WarmUp.js

--- Day4/WarmUp.js
+++ Day4/WarmUp.js
@@ -1,15 +1,17 @@
 
 
 function findWaldo(group) {
+    let newArr = [];
+    for(i = 0; i < group.length; i++){
 
-    for(i = 0; i <= group.length; i++){
-
-    if(group[i] === "Waldo"){
+    if(group[i] === 'Waldo'){
         group[i] = "Found Waldo!";
-    }}
-  
+        //newArr.push(group[i]);
+    
+
+  return group; }
 }
 
-let testVar = findWaldo(['Stacy', 'Waldo', 'John']);
-console.log(testVar); // returns ['Stacy', 'Found Waldo!', 'John']
+
+console.log(findWaldo(['Stacy', 'Waldo', 'John']);); // returns ['Stacy', 'Found Waldo!', 'John']
 findWaldo(['Waldo', 'Jimmy', 'James', 'Jamie', 'Waldo']) // ['Found Waldo!', 'Jimmy', 'James', 'Jamie', 'Found Waldo!']

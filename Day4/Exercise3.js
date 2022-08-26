0be3269fc601b43a3fdac9f0a9fc1a95637f5522
Crystal Campbell <blankpage@gmail.com>
2022-08-26 01:57:01 -0500

    FIX Clock in Exercise3

--- Day4/Exercise3.js
+++ Day4/Exercise3.js
@@ -1,22 +1,30 @@
 let date = new Date();
+let hoursPage = document.getElementById("hours");
+let minutesPage = document.getElementById("mins");
+let secondsPage = document.getElementById("seconds");
+let amPmPage = document.getElementById("amPm");
+
+let amPm = "AM";
+
+//Gets hours, minutes, seconds and updates clock.
 
 function showTime(){
+    let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
-    let amPm = "AM";
+    amPm = "AM";
+ 
+    hours = testHours(hours);
+    seconds = testSeconds(seconds);
+    minutes = testMinutes(minutes);
 
-    if(hours === 0){
-        hours = 12;
-    }
-
-    if(hours > 12){
-        hours = fixHours(hours);
-        amPm = "PM";
-    }
-
-    //setTimeout()
+    hoursPage.innerText = hours;
+    minutesPage.innerText = minutes;
+    secondsPage.innerText = seconds;
+    amPmPage.innerText = amPm;
 }
+
 
 //Change 24 hours to 12 - does not check if time is in 24 hour format
 
@@ -25,6 +33,49 @@ function fixHours(num){
     return num;
 }
 
+//Fixes hours formatting to 12 hour clock. 
+//Adds 0 if hours is single digit number
+// Changes amPm to PM if past noon
 
+function testHours(hourTest){
+    if(hourTest === 0){
+        hourTest = 12
+    }
 
-console.log(fixHours(14));
+    if(hourTest > 12) {
+        hourTest = fixHours(hourTest)
+        amPm = "PM";
+    }
+    
+    if(hourTest < 10){
+        hourTest = "0" + hourTest
+    }
+
+    return hourTest;
+
+}
+
+//Adds 0 if minutes is single digit number
+
+function testMinutes(minTest){
+    if(minTest < 10){
+        minTest = "0" + minTest
+    }
+return minTest;
+
+}
+
+///Adds 0 if minutes is single digit number
+
+function testSeconds(secTest){
+    if(secTest < 10){
+        secTest = "0" + secTest
+    }
+return secTest;
+
+}
+
+//Updates time every second
+setInterval(function () {
+    showTime();
+}, 1000);

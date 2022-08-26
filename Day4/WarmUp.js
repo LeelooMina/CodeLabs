07e019d4750c54a839651df1a023cb604641f2a0
Crystal Campbell <blankpage@gmail.com>
2022-08-26 00:21:14 -0500

    FIX waldo loop

--- Day4/WarmUp.js
+++ Day4/WarmUp.js
@@ -1,17 +1,29 @@
 
-
+//Function takes in array. Checks if array contains string "Waldo." Replaces "Waldo" with "Found Waldo!". Returns changed array.
 function findWaldo(group) {
-    let newArr = [];
-    for(i = 0; i < group.length; i++){
 
-    if(group[i] === 'Waldo'){
-        group[i] = "Found Waldo!";
-        //newArr.push(group[i]);
-    
+    let waldo = group.indexOf('Waldo');
 
-  return group; }
-}}
+   
+    if (waldo === -1){
+      return group;
+    }else{
+
+      for(let i = 0; i < group.length; i++){
+        if (group[i] === 'Waldo'){
+        group.splice(i, 1, 'Found Waldo!');
+       
+        }
+      }
+    }
+
+    return group;
+  }
 
 
-console.log(findWaldo(['Stacy', 'Waldo', 'John']); // returns ['Stacy', 'Found Waldo!', 'John']
-findWaldo(['Waldo', 'Jimmy', 'James', 'Jamie', 'Waldo']) // ['Found Waldo!', 'Jimmy', 'James', 'Jamie', 'Found Waldo!']
+
+console.log(findWaldo(['Stacy', 'Waldo', 'John'])); // returns ['Stacy', 'Found Waldo!', 'John']
+console.log(findWaldo(['Waldo', 'Jimmy', 'James', 'Jamie', 'Waldo'])); // ['Found Waldo!', 'Jimmy', 'James', 'Jamie', 'Found Waldo!']
+
+console.log(findWaldo([ 'Jimmy', 'James', 'Jamie'])); // ['Jimmy', 'James', 'Jamie']
+

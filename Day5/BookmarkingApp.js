1410fc111c279a03fe765f05a58628b3f59f4ecc
Crystal Campbell <blankpage@gmail.com>
2022-08-29 20:03:15 -0500

    ADD Bookmarking app

---
+++ Day5/BookmarkingApp.js
@@ -0,0 +1,50 @@
+//Get user input
+
+const btnSubmit = document.getElementById('submit');
+let bookmarks = [];
+
+//localStorage.getItem('bookmarks');
+
+btnSubmit.addEventListener("click", getBookmark);
+
+function getBookmark(){
+    
+    let bookMk = document.getElementById('bookmarkTx').value;
+    bookmarks.push(bookMk);
+    showBookmarks(bookmarks);
+
+console.log(bookmarks);
+    //return bookMk;
+
+}
+
+//append to body
+function showBookmarks(bmarkList){
+    for(i = 0; i <= bmarkList.length; i++){
+        
+    }
+
+}
+
+
+//for( ){
+
+//}
+
+//save to local storage
+
+
+
+//display saved bookmarks
+
+//delete all
+
+
+
+
+
+///getBookmark();
+
+
+
+

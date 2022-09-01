397958737de2caf2b9ef21310a4624d8a3aa246a
Crystal Campbell <blankpage@gmail.com>
2022-09-01 18:08:31 -0500

    Update warmup.js

--- Day6/warmup.js
+++ Day6/warmup.js
@@ -1,4 +1,5 @@
 function countTo(num){
+    console.log('Counting');
     for(i=0; i <= num; i++){
         console.log(i);
     }
@@ -9,6 +10,7 @@ countTo(5);
 countTo(10);
 
 function countEven(num){
+    console.log('Even');
     for(i=0; i <= num; i++){
         if(i % 2 === 0)
         console.log(i);
@@ -16,3 +18,45 @@ function countEven(num){
 }
 
 countEven(10);
+
+function countOdd(num){
+    console.log('Odd');
+    for(i=0; i <= num; i++){
+        if(i % 2 != 0)
+        console.log(i);
+    }
+}
+
+countOdd(10);
+
+function countPrime(num){
+    console.log('Prime');
+
+    for(i = 0; i <= num; i++)
+        //check = isPrime(i);
+        if(isPrime(i)){
+            console.log(i);
+        }
+        
+    }
+
+
+
+function isPrime(num) {
+    
+        if(num < 2){
+            return false;
+        }
+    
+        for (var i = 2; i < num; i++) {
+        
+        
+        if(num % i === 0){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+countPrime(10);

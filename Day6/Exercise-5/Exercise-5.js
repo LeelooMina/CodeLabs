edc780592c4aaf946eb9ddc927bd36a6c4f46e89
Crystal Campbell <blankpage@gmail.com>
2022-09-02 00:06:56 -0500

    Update exercises

--- Day6/Exercise-5/Exercise-5.js
+++ Day6/Exercise-5/Exercise-5.js
@@ -17,6 +17,31 @@ The resulting array's sort order is dependent on the input values.
 All inputs provided in the test scenarios are valid.
 If the start is greater than the end, return a descendingly sorted array; otherwise, ascendingly sorted. */
 
-function reversibleInclusiveList(){
+function reversibleInclusiveList(num1, num2){
+    let returnArr = [];
+
+    if(num1 < num2){
+        for (i = num1; i <= num2; i++){
+            returnArr.push(i);
+        }
+     } else{
+        for (i = num1; i >= num2; i--){
+            returnArr.push(i);
+        }
+
+    }
+
+    return returnArr;
+
     
 }
+
+console.log()
+
+console.log(reversibleInclusiveList(1, 5));    //[1, 2, 3, 4, 5]
+
+console.log(reversibleInclusiveList(2, 8));   // [2, 3, 4, 5, 6, 7, 8]
+
+console.log(reversibleInclusiveList(10, 20));    // [10, 11, 12, 13, 14, 15, 16, 17, 18, 1
+
+console.log(reversibleInclusiveList(24, 17));   // [24, 23, 22, 21, 20, 19, 18, 17]
--- Day6/Exercise-6/Exercise-6.js
+++ Day6/Exercise-6/Exercise-6.js
@@ -15,3 +15,38 @@ product([[1,2],[2,3],[3,4]])   ➞    20
 
 product([[1,2],[0,3],[3,0]])    ➞    2
 // 1 * 2 + 0 * 3 + 3 * 0 */
+
+
+function multiplyNums(arNum){
+    let sum = arNum[0] * arNum[1];
+
+    return sum;
+
+}
+
+function product(arNum1, arNum2, arNum3){
+
+    let sum1 = multiplyNums(arNum1);
+
+    let sum2 = multiplyNums(arNum2);
+
+    let sum3 = multiplyNums(arNum3);
+
+    return sum1 + sum2 + sum3;
+
+
+
+}
+
+
+console.log(product([1,2],[1,1],[2,3]))   //   9
+// 1 * 2 + 1 * 1 + 2 * 3//
+
+console.log(product([10,2],[5,0],[2,3]))  //    26
+// 10 * 2 + 5 * 0 + 2 * 3//
+
+console.log(product([1,2],[2,3],[3,4]))   //  20
+// 1 * 2 + 2 * 3 + 3 * 4//
+
+console.log(product([1,2],[0,3],[3,0]))   //   2
+// 1 * 2 + 0 * 3 + 3 * 0 */

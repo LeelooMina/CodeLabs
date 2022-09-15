0ca6b87f58226f554e14ada3d66a7b9b3a217e9f
Crystal Campbell <blankpage@gmail.com>
2022-09-15 08:26:16 -0500

    ADD Day 8 exercises

--- Day8/exercise-2/asciiCap.js
+++ Day8/exercise-2/asciiCap.js
@@ -9,21 +9,31 @@ asciiCapitalize("THE LITTLE MERMAID")    ➞    "THe LiTTLe meRmaiD"
 asciiCapitalize("Oh what a beautiful morning.")    ➞    "oH wHaT a BeauTiFuL moRNiNg."*/
 
 function asciiCapitalize(string){
-    for(i = 0; i <= string.length; i++){
-        if (string.charCodeAt(i) % 2 === 0){
-            
-        }
     
-    }
-    arr = string.split("");
+    let returnVal = capOrNot(string);
+    return returnVal;
     
-    arr.forEach()
-
-    console.log(arr);
 }
 
 function capOrNot(test){
+    let returnString = ""
+
+    for(i = 0; i < test.length; i++){
+        if (test.charCodeAt(i) % 2 === 0){
+            returnString += test[i].toUpperCase();
+        }
+    else{
+            returnString += test[i].toLowerCase();
+        }
+    
+    }
+
+    return returnString;
 
 }
 
-asciiCapitalize("to be or not to be!");
+console.log(asciiCapitalize("to be or not to be!"));
+
+console.log(asciiCapitalize("THE LITTLE MERMAID"));
+
+console.log(asciiCapitalize("Oh what a beautiful morning."));
---
+++ Day8/exercise-3/exercise3.js
@@ -0,0 +1,70 @@
+// Aim: Create a function to check if a candidate is qualified in an imaginary coding interview of an imaginary tech startup.
+
+// The criteria for a candidate to be qualified in the coding interview is:
+
+// The candidate should have completed all the questions.
+// The maximum time given to complete the interview is 120 minutes.
+// The maximum time given for very easy questions is 5 minutes each.
+// The maximum time given for easy questions is 10 minutes each.
+// The maximum time given for medium questions is 15 minutes each.
+// The maximum time given for hard questions is 20 minutes each.
+// If all the above conditions are satisfied, return "qualified", else return "disqualified".
+
+// You will be given an array of time taken by a candidate to solve a particular question and the total time the candidate takes to complete the interview.
+
+// Given an array, in a true condition will always be in the format [very easy, very easy, easy, easy, medium, medium, hard, hard].
+
+// The maximum time to complete the interview includes a buffer time of 20 minutes.
+
+// Examples
+
+// interview([5, 5, 10, 10, 15, 15, 20, 20], 120)    ➞    "qualified"
+
+// interview([2, 3, 8, 6, 5, 12, 10, 18], 64)    ➞     "qualified"
+
+// interview([5, 5, 10, 10, 25, 15, 20, 20], 120)    ➞    "disqualified"
+// // Exceeded the time limit for a medium question.
+
+// interview([5, 5, 10, 10, 15, 15, 20], 120)    ➞    "disqualified"
+// // Did not complete all the questions.
+
+// interview([5, 5, 10, 10, 15, 15, 20, 20], 130)    ➞    "disqualified"
+// // Solved all the questions in their respected time limits but exceeded the total time limit of the interview.
+// Notes
+
+// Try to solve the problem using only array methods.
+
+function interview(arr, time){
+    if (time > 120 || arr.length < 8){ //Test time and number of questions answered
+        return "disqualified";
+    }
+    else if ( arr[0] > 5 || arr[1] > 5){ //Very easy questions
+        return "disqualified";
+    }
+    else if ( arr[2] > 10 || arr[3] > 10){ //Easy questions
+        return "disqualified";
+    }
+    else if ( arr[4] > 15 || arr[5] > 15){ //Medium questions
+        return "disqualified";
+    }
+    else if ( arr[6] > 20 || arr[7] > 20){ //Hard questions
+        return "disqualified";
+    }
+    else {
+        return "qualified";
+    }
+      
+}
+
+console.log(interview([5, 5, 10, 10, 15, 15, 20, 20], 120));    //➞    "qualified"
+
+console.log(interview([2, 3, 8, 6, 5, 12, 10, 18], 64));//➞     "qualified"
+
+console.log(interview([5, 5, 10, 10, 25, 15, 20, 20], 120));    //➞    "disqualified"
+// Exceeded the time limit for a medium question.
+
+console.log(interview([5, 5, 10, 10, 15, 15, 20], 120));    // ➞    "disqualified"
+// Did not complete all the questions.
+
+console.log(interview([5, 5, 10, 10, 15, 15, 20, 20], 130));    // ➞    "disqualified"
+// Solved all the questions in their respected time limits but exceeded the total time limit of the interview.

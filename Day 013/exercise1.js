95feb2641f29daab36375abf96a67d7e8076e34d
Crystal Campbell <blankpage@gmail.com>
2022-10-03 17:59:16 -0500

    ADD: Day 13 Exercises

---
+++ Day 013/exercise1.js
@@ -0,0 +1,18 @@
+// Use a loop to print the following to the console:
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// let stars = "*"
+
+for (i = 0; i < 5; i++) {
+
+    // console.log(stars);
+    // stars += "*";
+
+    console.log(stars.repeat(i+1))
+
+}
---
+++ Day 013/exercise2.js
@@ -0,0 +1,41 @@
+// Use a loop to print the following to the console:
+
+// ---*---
+// --***--
+// -*****-
+
+
+for(i = 3, s = 1; i > 0; i--, s+=2){
+    console.log("-".repeat(i)+"*".repeat(s)+"-".repeat(i));
+   
+}
+
+
+
+
+
+
+// let tree = "--*--"
+
+// function tree(string){
+    
+    
+//     // for (i = 0; i < 5; i++) {
+    
+//     if(string.charAt(2) === "-"){
+//         return "--***--";
+//     }
+//     if(string.charAt(1) === "-"){
+//         return "-*****-";
+//     }
+//     return string;
+    
+//     //console.log(tree);
+   
+
+// }
+
+// function makeTree(){
+//     console.log(tree("--*--"));
+//     console.log()
+// }

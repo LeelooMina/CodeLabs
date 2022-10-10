9d265fe8cd835ec327fe9f9953ed2d23d9032a5d
Crystal Campbell <blankpage@gmail.com>
2022-10-10 17:56:55 -0500

    ADD: Day 16

---
+++ Day 016/warmup.js
@@ -0,0 +1,3 @@
+// Given numbers 0 - 100;
+
+// For every number that is a multiple of 3, log to the console "Fizz". For every number that is a multiple of 5, log to the console "Buzz". For every number that is a multiple of 5 and 3, log to the console "Fizz Buzz".

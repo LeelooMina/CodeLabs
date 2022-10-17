d4b40793e53c7e8d161625f0e50bad0b457a0b44
Crystal Campbell <blankpage@gmail.com>
2022-10-17 17:54:37 -0500

    Update Day 18

--- Day 018/exercise-2.js
+++ Day 018/exercise-2.js
@@ -1,5 +1,8 @@
-function kmToMi (km) {
-    return km * 0.62137;
+function kmToMi () {
+   let km = document.getElementById("userInput").value
+   let result = km * 0.62137;
+  
+   document.getElementById("conversion").innerText(`${result}`)
 }
 
 
